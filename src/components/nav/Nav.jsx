7f00ef18903e14c49334db4a3c038f15eb5ab4cd
Armando <A01253219@tec.mx>
2023-04-01 21:15:57 -0700

    Added about, navbar and other stuff

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBriefcase} from 'react-icons/bs'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {CgGames} from 'react-icons/cg'
+import { useState } from 'react'
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsBriefcase/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+      <a href="#games" onClick={() => setActiveNav('#games')} className={activeNav === '#games' ? 'active' : ''}><CgGames/></a>
+    </nav>
+    
   )
 }
 
